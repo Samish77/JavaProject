6cfc69c2b475745a4ecc07e28a4d4d6920251a99
sam <BusinessComputers.in@192.168.100.8>
2022-09-25 19:50:12 +0530

    comit1

---
+++ Java_class_Object/src/Inheritance1.java
@@ -0,0 +1,21 @@
+
+class parent{
+	int n=2000;
+}
+
+
+
+
+
+class Inheritance1 extends parent {
+	int j=1000;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Inheritance1 s1=new Inheritance1();
+
+System.out.println("Parent value: " +s1.n );
+System.out.println("child value: " +s1.j);
+	}
+
+}
